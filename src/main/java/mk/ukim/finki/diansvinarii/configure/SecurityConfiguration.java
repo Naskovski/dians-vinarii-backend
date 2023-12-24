c6c0acf1744116770d26cb90ffb4a3d058f75a26
Naskovski <filip.naskovski.hk@gmail.com>
2023-12-24 23:09:13 +0100

    Dockerfile added

--- src/main/java/mk/ukim/finki/diansvinarii/configure/SecurityConfiguration.java
+++ src/main/java/mk/ukim/finki/diansvinarii/configure/SecurityConfiguration.java
@@ -23,21 +23,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
-@Configuration
-class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3005");
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-}
+
 
 @Configuration
 @EnableWebSecurity
